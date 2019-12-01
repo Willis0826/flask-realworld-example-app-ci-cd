f9be57f3263e0895771cb9ce15ea2b2f6bbd92bb
Willis Chou (EXT-TW) <willis_chou@trendmicro.com>
2019-12-02 00:19:05 +0800

    Add test coverage

--- conduit/commands.py
+++ conduit/commands.py
@@ -18,7 +18,7 @@ TEST_PATH = os.path.join(PROJECT_ROOT, 'tests')
 def test():
     """Run the tests."""
     import pytest
-    rv = pytest.main([TEST_PATH, '--verbose'])
+    rv = pytest.main([TEST_PATH, '--verbose', '--cov', 'conduit'])
     exit(rv)
 
 
